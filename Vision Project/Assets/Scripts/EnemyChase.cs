b2cde24a3211143ee90382cefca93c84adf8525c
James-Mc-E <77745581+James-Mc-E@users.noreply.github.com>
2021-05-07 16:00:26 +0100

    Multiple additions

---
+++ Vision Project/Assets/Scripts/EnemyChase.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EnemyChase : MonoBehaviour
+{
+
+    public Transform target;//set target from inspector instead of looking in Update
+    public float speed;
+    public float distance;
+    public float agroRange;
+
+
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, target.position) < agroRange) //Agro range
+        {  //rotate to look at the player
+            transform.LookAt(target.position);
+            transform.Rotate(new Vector3(0, -90, 0), Space.Self);//correcting the original rotation
+        }
+
+        if (Vector3.Distance(transform.position, target.position) < agroRange) //Agro range
+        {   //move towards the player
+            if (Vector3.Distance(transform.position, target.position) > distance)
+            {//move if distance from target is greater than distance
+                transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            }
+        }
+    }
+}
